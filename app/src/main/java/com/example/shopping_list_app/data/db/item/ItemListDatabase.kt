b689817acc97aef84cae00fc162400143db0791a
Ozan <ozancangu24@gmail.com>
2023-03-25 20:04:50 +0300

    -Itemlist database updated

--- app/src/main/java/com/example/shopping_list_app/data/db/item/ItemListDatabase.kt
+++ app/src/main/java/com/example/shopping_list_app/data/db/item/ItemListDatabase.kt
@@ -3,11 +3,16 @@ package com.example.shopping_list_app.data.db.item
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.shopping_list_app.data.db.history.HistoryDao
+import com.example.shopping_list_app.data.db.history.HistoryItem
 
-@Database(entities = [Item::class], version = 1,
+@Database(entities = [Item::class, HistoryItem::class], version = 1,
     exportSchema = false,)
 
 @TypeConverters(TypeConverter::class)
 abstract class ItemListDatabase: RoomDatabase(){
     abstract fun itemDao():ItemDao
+    abstract fun historyDao(): HistoryDao
 }
+
+
