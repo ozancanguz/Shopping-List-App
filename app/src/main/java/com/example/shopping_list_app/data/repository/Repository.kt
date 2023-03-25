291dbaa0a8a7d8a5c95642e06d4480147b007764
Ozan <ozancangu24@gmail.com>
2023-03-25 18:03:41 +0300

    -Repository created and dao injected

---
+++ app/src/main/java/com/example/shopping_list_app/data/repository/Repository.kt
@@ -0,0 +1,29 @@
+package com.example.shopping_list_app.data.repository
+
+import androidx.lifecycle.LiveData
+import com.example.shopping_list_app.data.db.item.Item
+import com.example.shopping_list_app.data.db.item.ItemDao
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
+
+@ViewModelScoped
+class Repository@Inject constructor(private val itemDao: ItemDao) {
+
+
+    fun getAllItems(): LiveData<List<Item>> {
+        return itemDao.getAllItems()
+    }
+
+    suspend fun insertItem(item: Item){
+        return itemDao.insertData(item)
+    }
+
+    suspend fun deleteItem(item: Item){
+        return itemDao.delete(item)
+    }
+
+    suspend fun deleleAllItems(){
+        return itemDao.deleteAll()
+    }
+
+}
