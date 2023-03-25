5317f2bf42e097b1ea439527fd8f203bd4f50316
Ozan <ozancangu24@gmail.com>
2023-03-25 18:04:34 +0300

    -Itemlist view model created

---
+++ app/src/main/java/com/example/shopping_list_app/viewmodel/ItemListViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.shopping_list_app.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.shopping_list_app.data.db.item.Item
+import com.example.shopping_list_app.data.repository.Repository
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ItemListViewModel @Inject constructor(private val repository: Repository, application: Application):AndroidViewModel(application){
+
+
+    // get all items
+    fun getAllItems(){
+        repository.getAllItems()
+    }
+
+    // insert item
+    fun insertData(item: Item){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.insertItem(item)
+        }
+    }
+
+    // delete single item
+    fun deleteItem(item: Item){
+        viewModelScope.launch (Dispatchers.IO){
+            repository.deleteItem(item)
+        }
+    }
+
+    // delete all
+    fun deleteAll(){
+        viewModelScope.launch (Dispatchers.IO){
+            repository.deleleAllItems()
+        }
+    }
+
+
+
+}
