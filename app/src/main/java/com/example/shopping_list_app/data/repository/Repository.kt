15c8b6f109064fc3d710f70b4867fb9eeda1ce06
Ozan <ozancangu24@gmail.com>
2023-03-25 20:06:01 +0300

    -repository updated for history item

--- app/src/main/java/com/example/shopping_list_app/data/repository/Repository.kt
+++ app/src/main/java/com/example/shopping_list_app/data/repository/Repository.kt
@@ -1,13 +1,15 @@
 package com.example.shopping_list_app.data.repository
 
 import androidx.lifecycle.LiveData
+import com.example.shopping_list_app.data.db.history.HistoryDao
+import com.example.shopping_list_app.data.db.history.HistoryItem
 import com.example.shopping_list_app.data.db.item.Item
 import com.example.shopping_list_app.data.db.item.ItemDao
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 @ViewModelScoped
-class Repository@Inject constructor(private val itemDao: ItemDao) {
+class Repository@Inject constructor(private val itemDao: ItemDao,private val historyDao: HistoryDao) {
 
 
     fun getAllItems(): LiveData<List<Item>> {
@@ -24,6 +26,23 @@ class Repository@Inject constructor(private val itemDao: ItemDao) {
 
     suspend fun deleleAllItems(){
         return itemDao.deleteAll()
+    }
+
+
+
+    fun getAllHistoryItems():LiveData<List<HistoryItem>>{
+        return historyDao.getAllHistoryItems()
+    }
+
+    suspend fun insertHistoryItem(historyItem: HistoryItem){
+        return historyDao.insertHistoryItem(historyItem)
+    }
+
+    suspend fun deleteHistoryItem(historyItem: HistoryItem){
+        return historyDao.deleteHistoryItem(historyItem)
+    }
+    suspend fun deleteAllHistoryItems(){
+        return historyDao.deleteAllHistoryItem()
     }
 
 
