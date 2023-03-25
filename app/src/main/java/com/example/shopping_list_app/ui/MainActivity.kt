be6c641b135c8e2e8974d01aa5519efca1d0802a
Ozan <ozancangu24@gmail.com>
2023-03-25 17:10:57 +0300

    -bottom navigation created

--- app/src/main/java/com/example/shopping_list_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/shopping_list_app/ui/MainActivity.kt
@@ -4,9 +4,41 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.shopping_list_app.R
 
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+
+
+import androidx.navigation.ui.AppBarConfiguration
+
+
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.shopping_list_app.databinding.ActivityMainBinding
+
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.itemListFragment,
+                R.id.historyFragment
+            )
+        )
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
