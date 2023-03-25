fd1fad93f0e759f099fcb3683121bb4ad99598f8
Ozan <ozancangu24@gmail.com>
2023-03-25 17:59:08 +0300

    -ItemDao.kt created

---
+++ app/src/main/java/com/example/shopping_list_app/data/db/item/ItemDao.kt
@@ -0,0 +1,25 @@
+package com.example.shopping_list_app.data.db.item
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ItemDao {
+
+    @Query("select * from itemlist_table order by id Asc")
+    fun getAllItems(): LiveData<List<Item>>
+
+
+
+    // insert item
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertData(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+
+    @Query("delete from itemlist_table")
+    suspend fun deleteAll()
+
+
+}
