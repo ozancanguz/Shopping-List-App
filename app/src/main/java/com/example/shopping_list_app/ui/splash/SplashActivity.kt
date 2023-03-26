f4d215a75c3c8ab4a640e3c775ead59194e8c570
Ozan <ozancangu24@gmail.com>
2023-03-26 19:09:13 +0300

    -splash screen added

---
+++ app/src/main/java/com/example/shopping_list_app/ui/splash/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.example.shopping_list_app.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shopping_list_app.databinding.ActivitySplashBinding
+import com.example.shopping_list_app.ui.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    //threaad 5 sn yani 5000 ms uyusun
+                    Thread.sleep(5000)
+                    //intent ile splash ekranından sonra MainActivity ekranı açılsın diyoruz
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                }catch (e : Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+
+
+}
